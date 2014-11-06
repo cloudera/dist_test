29e9b4d9ee17e2c54f47f767c1cfa6084ad70aca
Andrew Wang <andrew.wang@cloudera.com>
2014-11-06 13:25:16 -0800

    Add lib dir for additional deps

--- generate_isolate.py
+++ generate_isolate.py
@@ -9,16 +9,19 @@ if len(sys.argv) != 2:
     sys.exit(1)
 
 dist_root = sys.argv[1]
+dist_glob = dist_root + "/target/hadoop-[0-9]*"
 
 # Find folders with jars and add to the classpath
-cmd="""find %s -name *.jar | sed "s|[^/]*.jar||" | sort -u""" % (dist_root)
+cmd="""find %s -name *.jar | sed "s|[^/]*.jar||" | sort -u""" % (dist_glob)
 jar_folders = subprocess.check_output(cmd, shell=True).split("\n")[:-1]
 
 # Format this as a classpath
-cmd="""find %s -name *.jar | sed "s|[^/]*.jar|\*|" | sort -u | tr '\n' :""" % (dist_root)
+cmd="""find %s -name *.jar | sed "s|[^/]*.jar|\*|" | sort -u | tr '\n' :""" % (dist_glob)
+classpath = jar_folders.replace(
 classpath = subprocess.check_output(cmd, shell=True)
 
-test_class = "org.apache.hadoop.hdfs.server.namenode.TestAddBlock"
+#test_class = "org.apache.hadoop.hdfs.server.namenode.TestAddBlock"
+test_class = "org.apache.hadoop.fs.TestFileStatus"
 
 run_test_cmd = "run_junit"
 
