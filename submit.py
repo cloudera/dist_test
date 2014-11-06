f14449d0d3c72324b0bb4ae4f675bd3b66c70ba7
Todd Lipcon <todd@cloudera.com>
2014-11-06 11:00:39 -0800

    Fix some ANSI sequences

--- submit.py
+++ submit.py
@@ -12,20 +12,38 @@ import time
 
 TEST_MASTER = "http://a1228.halxg.cloudera.com:8081"
 
+RED = "\x1b[31m"
+RESET = "\x1b[m"
+
 def generate_job_id():
   return "%s.%d.%d" % (getpass.getuser(), int(time.time()), os.getpid())
 
 def watch_results(job_id):
   url = TEST_MASTER + "/job_status?" + urllib.urlencode([("job_id", job_id)])
   start_time = time.time()
+
+  first = True
   while True:
     result_str = urllib2.urlopen(url).read()
     result = simplejson.loads(result_str)
-    print "\x1b[F\x1b[2K",
+
+    # On all but the first iteration, replace the previous line
+    if not first:
+      print "\x1b[F\x1b[2K",
+
+    first = False
     run_time = time.time() - start_time
-    print "%.1fs\t%d/%d tasks complete (%d failed)" % \
-        (run_time,
-         result['finished_tasks'], result['total_tasks'], result['failed_tasks'])
+    print "%.1fs\t" % run_time,
+
+    print "%d/%d tasks complete" % \
+        (result['finished_tasks'], result['total_tasks']),
+
+    if result['failed_tasks']:
+      print RED,
+      print "(%d failed)" % result['failed_tasks'],
+      print RESET,
+    print
+
     if result['finished_tasks'] == result['total_tasks']:
       break
     time.sleep(0.5)
