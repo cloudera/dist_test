767d28bd6c889d0b56913aec1c560b1c5ba283a9
Andrew Wang <andrew.wang@cloudera.com>
2015-08-18 17:36:55 -0700

    Add and fix enumerator tests

--- python/disttest/enumerators.py
+++ python/disttest/enumerators.py
@@ -9,14 +9,6 @@ class Enumerator:
     def __init__(self, directory):
         self.directory = directory
         self.modules_to_classes = {}
-        self.__walk()
-        self.__filter()
-
-    def __walk(self):
-        pass
-
-    def __filter():
-        pass
 
     def get_modules(self):
         return self.modules_to_classes.keys()
@@ -24,37 +16,48 @@ class Enumerator:
     def get_modules_to_classes(self):
         return self.modules_to_classes
 
-def PatternEnumerator(Enumerator):
+class PatternEnumerator(Enumerator):
 
     def __init__(self, directory):
-        Enumerator.__init__(directory)
+        Enumerator.__init__(self, directory)
+        #self.filters = [AnyFileFilter(), PotentialTestClassNameFilter(), NoAbstractClassFilter()]
+        self.filters = [AnyFileFilter(), PotentialTestClassNameFilter()]
+        self._walk()
 
-    def __walk(self):
+    def add_filter(self, f):
+        return self.modules_to_classes.keys()
+
+    def get_modules_to_classes(self):
+        return self.modules_to_classes
+
+class PatternEnumerator(Enumerator):
+
+    def __init__(self, directory):
+        Enumerator.__init__(self, directory)
+        #self.filters = [AnyFileFilter(), PotentialTestClassNameFilter(), NoAbstractClassFilter()]
+        self.filters = [AnyFileFilter(), PotentialTestClassNameFilter()]
+        self._walk()
+
+    def add_filter(self, f):
+        self.filters += [f]
+
+    def _walk(self):
         # Find the modules first, directories that have a pom.xml and a target dir
-        for root, dirs, files in os.walk(directory):
-            if files.contains("pom.xml") and dirs.contains("target"):
+        for root, dirs, files in os.walk(self.directory):
+            if "pom.xml" in files and "target" in dirs:
                 self.modules_to_classes[root] = []
         # For each module, look for test classes within target dir
         for module in self.modules_to_classes.keys():
+            module_dir = os.path.join(module, "target")
+            print "Traversing module", module_dir
             for root, dirs, files in os.walk(os.path.join(module, "target")):
-                for f in files:
-                    # Only class files
-                    if not f.endswith(".class"):
-                        continue
-                    # No nested classes
-                    if f.contains("$"):
-                        continue
-                    # Match against default Surefire pattern
-                    name = f[:-len(".class")]
-                    if not name.startswith("Test") and \
-                       not name.endswith("Test") and \
-                       not name.endswith("TestCase"):
-                        continue
-                    modules_to_classes[module].append(os.path.join(root, f))
-
-    def __filter(self):
-        for k, v in modules_to_classes.iteritems():
-            v = [x for x in v if NoAbstractFilter.accept(v)]
+                # Apply all the filters
+                filtered = [os.path.join(root, f) for f in files]
+                for fil in self.filters:
+                    filtered = [x for x in filtered if fil.accept(x)]
+                # Add filtered files to dict
+                for f in filtered:
+                    self.modules_to_classes[module].append(os.path.join(root, f))
 
 class FileFilter:
     def accept(self, f):
@@ -64,7 +67,24 @@ class AnyFileFilter(FileFilter):
     def accept(self, f):
         return os.path.isfile(f)
 
-class NoAbstractFilter(FileFilter):
+class PotentialTestClassNameFilter(FileFilter):
+    def accept(self, f):
+        f = os.path.basename(f)
+        # Only class files
+        if not f.endswith(".class"):
+            return False
+        # No nested classes
+        if "$" in f:
+            return False
+        # Match against default Surefire pattern
+        name = f[:-len(".class")]
+        if not name.startswith("Test") and \
+           not name.endswith("Test") and \
+           not name.endswith("TestCase"):
+            return False
+        return True
+
+class NoAbstractClassFilter(FileFilter):
     def accept(self, f):
         clazz = classfile.Classfile(f)
         return not (clazz.is_interface() or clazz.is_abstract())
--- python/disttest/test/test.py
+++ python/disttest/test/test.py
@@ -5,10 +5,16 @@ import unittest
 
 from .. import enumerators
 
+TEST_RESOURCES = os.path.join(os.path.abspath(os.path.dirname(__file__)), "test-resources")
+
 class TestEnumerators(unittest.TestCase):
 
     def test_pattern_enumerator(self):
-        enumerator = enumerators.PatternEnumerator("/tmp")
+        enum = enumerators.PatternEnumerator("/home/andrew/dev/hadoop/trunk/hadoop-common-project/hadoop-kms")
+        for module, classes in enum.get_modules_to_classes().iteritems():
+            print module
+            for c in classes:
+                print c
 
 class TestFilters(unittest.TestCase):
 
@@ -24,18 +30,17 @@ class TestFilters(unittest.TestCase):
         self.assertFalse(any_filter.accept(os.path.join(self.temp, "nosuchfileexists")))
         self.assertTrue(any_filter.accept(os.path.join(self.temp, "foofile")))
 
-    def test_NoAbstractFilter(self):
-        noabs_filter = enumerators.NoAbstractFilter()
+    def test_NoAbstractClassFilter(self):
+        noabs_filter = enumerators.NoAbstractClassFilter()
         num_files = 0
-        for root, dirs, files in os.walk(os.path.dirname(__file__), "/test-resources/classes/"):
+        for root, dirs, files in os.walk(TEST_RESOURCES, "classes/"):
             for f in files:
                 fullpath = os.path.realpath(os.path.join(root, f))
-                if f.endswith(".class") and "/test-classes/" in fullpath and not "$" in fullpath:
-                    if "/abstract/" in fullpath:
-                        self.assertFalse(noabs_filter.accept(fullpath))
-                    elif "/concrete/" in fullpath:
-                        self.assertTrue(noabs_filter.accept(fullpath))
-                    num_files += 1
+                if root.endswith("abstract"):
+                    self.assertFalse(noabs_filter.accept(fullpath), "Path %s is abstract!" % fullpath)
+                else:
+                    self.assertTrue(noabs_filter.accept(fullpath), "Path %s is not abstract!" % fullpath)
+                num_files += 1
 
         print "Filtered %s files" % num_files
 
