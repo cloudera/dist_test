fc1f5e43ff0e00c16d02d09d154a5603cbd8224b
Andrew Wang <andrew.wang@cloudera.com>
2015-10-23 15:57:14 -0700

    Improve client.py log messages in Jenkins environments
    * Avoid printing color codes
    * Do not print unless the result has changed
    * Print a success/fail message at the end too, colorized

--- client.py
+++ client.py
@@ -19,10 +19,59 @@ TEST_MASTER = config.DIST_TEST_MASTER
 
 RED = "\x1b[31m"
 YELLOW = "\x1b[33m"
+GREEN = "\x1b[32m"
 RESET = "\x1b[m"
 
 def generate_job_id():
   return "%s.%d.%d" % (getpass.getuser(), int(time.time()), os.getpid())
+
+def print_status(start_time, previous_result, result,
+                 interactive=False, first=False):
+  # In non-interactive mode, do not print unless the result changed
+  if not interactive and previous_result is not None:
+    if previous_result['finished_tasks'] == result['finished_tasks']:
+      return
+
+  # In interactive mode, delete the previous line of output after the first
+  if interactive and not first:
+    print "\x1b[F\x1b[2K",
+
+  run_time = time.time() - start_time
+  print "%.1fs\t" % run_time,
+
+  print "%d/%d tasks complete" % \
+      (result['finished_tasks'], result['total_tasks']),
+
+  if result['failed_groups']:
+    p = "(%d failed)" % result['failed_groups']
+    if interactive:
+      print RED, p, RESET,
+    else:
+      print p,
+
+  if result['retried_tasks']:
+    p = "(%d retried)" % result['retried_tasks']
+    if interactive:
+      print YELLOW, p, RESET,
+    else:
+      print p,
+
+  print
+
+def print_exit(retcode, interactive=False):
+  success_msg = "Success! :D"
+  failure_msg = "Failure :'("
+
+  if retcode == 0:
+    if interactive:
+      print GREEN + success_msg + RESET
+    else:
+      print success_msg
+  else:
+    if interactive:
+      print RED + failure_msg + RESET
+    else:
+      print failure_msg
 
 def do_watch_results(job_id):
   watch_url = TEST_MASTER + "/job?" + urllib.urlencode([("job_id", job_id)])
@@ -31,39 +80,33 @@ def do_watch_results(job_id):
   url = TEST_MASTER + "/job_status?" + urllib.urlencode([("job_id", job_id)])
   start_time = time.time()
 
+  # We can detect a non-interactive Jenkins environment by checking for BUILD_ID
+  interactive = "BUILD_ID" not in os.environ.keys()
   first = True
+  previous_result = None
   while True:
     result_str = urllib2.urlopen(url).read()
     result = simplejson.loads(result_str)
 
-    # On all but the first iteration, replace the previous line
-    if not first:
-      print "\x1b[F\x1b[2K",
-
-    first = False
-    run_time = time.time() - start_time
-    print "%.1fs\t" % run_time,
-
-    print "%d/%d tasks complete" % \
-        (result['finished_tasks'], result['total_tasks']),
-
-    if result['failed_groups']:
-      print RED,
-      print "(%d failed)" % result['failed_groups'],
-      print RESET,
-
-    if result['retried_tasks']:
-      print YELLOW,
-      print "(%d retried)" % result['retried_tasks'],
-      print RESET,
-
-    print
-
-    # All done, return a UNIX return code according to test result
+    # Set the UNIX return code if we're finished, according to the test result
+    retcode = None
     if result['finished_tasks'] == result['total_tasks']:
       if result['failed_groups'] > 0:
-        return 88
-      return 0
+        retcode = 88
+      else:
+        retcode = 0
+
+    print_status(start_time, previous_result, result, interactive=interactive, first=first)
+    first = False
+
+    previous_result = result
+
+    # Return if we're done
+    if retcode is not None:
+      print_exit(retcode, interactive=interactive)
+      return retcode
+
+    # Sleep until next interval
     time.sleep(0.5)
 
 def save_last_job_id(job_id):
