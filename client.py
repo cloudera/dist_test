ed8dc52bdc0367777fa0925507ae8bc08c689ee0
Andrew Wang <andrew.wang@cloudera.com>
2015-10-28 16:57:32 -0700

    Convert client.py to use own logger
    This has the same motivation as converting the server and slave to use their
    own loggers. Not doing this can lead to simply not getting any log prints.

--- client.py
+++ client.py
@@ -22,6 +22,9 @@ YELLOW = "\x1b[33m"
 GREEN = "\x1b[32m"
 RESET = "\x1b[m"
 
+LOG = logging.getLogger('dist_test.client')
+LOG.setLevel(logging.INFO)
+
 def generate_job_id():
   return "%s.%d.%d" % (getpass.getuser(), int(time.time()), os.getpid())
 
@@ -75,7 +78,7 @@ def print_exit(retcode, interactive=False):
 
 def do_watch_results(job_id):
   watch_url = TEST_MASTER + "/job?" + urllib.urlencode([("job_id", job_id)])
-  logging.info("Watch your results at %s", watch_url)
+  LOG.info("Watch your results at %s", watch_url)
 
   url = TEST_MASTER + "/job_status?" + urllib.urlencode([("job_id", job_id)])
   start_time = time.time()
@@ -137,7 +140,7 @@ def submit_job_json(job_prefix, json):
 
   save_last_job_id(job_id)
 
-  logging.info("Submitted job %s", job_id)
+  LOG.info("Submitted job %s", job_id)
   return job_id
 
 def submit(argv):
@@ -162,7 +165,7 @@ def get_job_id_from_args(command, args):
   if len(args) == 1:
     job_id = load_last_job_id()
     if job_id is not None:
-      logging.info("Using most recently submitted job id: %s" % job_id)
+      LOG.info("Using most recently submitted job id: %s" % job_id)
       return job_id
   if len(args) != 2:
     print >>sys.stderr, "usage: %s %s <job-id>" % (os.path.basename(sys.argv[0]), command)
@@ -201,10 +204,10 @@ def fetch(argv):
 
   failed_tasks = fetch_failed_tasks(job_id)
   if len(failed_tasks) == 0:
-    logging.info("No failed tasks in provided job, or job does not exist")
+    LOG.info("No failed tasks in provided job, or job does not exist")
     return
 
-  logging.info("Fetching %d failed task logs into %s",
+  LOG.info("Fetching %d failed task logs into %s",
                len(failed_tasks),
                options.out_dir)
   try:
@@ -217,23 +220,23 @@ def fetch(argv):
     if 'stdout_link' in t:
       path = path_prefix + ".stdout"
       if not os.path.exists(path):
-        logging.info("Fetching stdout for task %s into %s", t['task_id'], path)
+        LOG.info("Fetching stdout for task %s into %s", t['task_id'], path)
         urllib.urlretrieve(t['stdout_link'], path)
     else:
-      logging.info("No stdout for task %s" % t['task_id'])
+      LOG.info("No stdout for task %s" % t['task_id'])
     if 'stderr_link' in t:
       path = path_prefix + ".stderr"
       if not os.path.exists(path):
-        logging.info("Fetching stderr for task %s into %s", t['task_id'], path)
+        LOG.info("Fetching stderr for task %s into %s", t['task_id'], path)
         urllib.urlretrieve(t['stderr_link'], path)
     else:
-      logging.info("No stderr for task %s" % t['task_id'])
+      LOG.info("No stderr for task %s" % t['task_id'])
 
 def cancel_job(argv):
   job_id = get_job_id_from_args("cancel", argv)
   url = TEST_MASTER + "/cancel_job?" + urllib.urlencode([("job_id", job_id)])
   result_str = urllib2.urlopen(url).read()
-  logging.info("Cancellation: %s" % result_str)
+  LOG.info("Cancellation: %s" % result_str)
 
 def usage(argv):
   print >>sys.stderr, "usage: %s <command> [<args>]" % os.path.basename(argv[0])
@@ -245,7 +248,6 @@ def usage(argv):
   print >>sys.stderr, "%s <command> --help may provide further info" % argv[0]
 
 def main(argv):
-  logging.basicConfig(level=logging.INFO)
   if len(argv) < 2:
     usage(argv)
     sys.exit(1)
