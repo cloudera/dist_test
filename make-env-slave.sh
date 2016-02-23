7dd13b74369183d1f44bf155426e12f80da6e94e
Andrew Wang <andrew.wang@cloudera.com>
2016-02-22 18:58:14 -0800

    Add GCE-related libs to make-env-slave.sh virtualenv

--- make-env-slave.sh
+++ make-env-slave.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 virtualenv slave-env
-for package in beanstalkc MySQL-python boto glob2 ; do
-  ./slave-env/bin/pip install $package
+for package in beanstalkc MySQL-python boto glob2 google-api-python-client oauth2client==1.5.2; do
+  ./slave-env/bin/pip install --upgrade $package
 done
 virtualenv --relocatable slave-env
