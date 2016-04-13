aafdf39f5d820b8e3cd932f2d0555cf5f64ee96b
Andrew Wang <andrew.wang@cloudera.com>
2016-04-13 15:24:11 -0700

    Add bash wrappers that init and activate virtualenv.

--- make-env-slave.sh
+++ make-env-slave.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 virtualenv slave-env
-for package in beanstalkc MySQL-python boto glob2 ; do
+for package in beanstalkc MySQL-python boto glob2 PyYAML; do
   ./slave-env/bin/pip install --upgrade $package
 done
 virtualenv --relocatable slave-env
